e27475ce39d19811a0ba07292ee947ae5d10c58b
8bit-pixies <2498638+8bit-pixies@users.noreply.github.com>
2024-03-24 13:13:23 +1100

    Update test_func_def_check.py

--- tests/test_func_def_check.py
+++ tests/test_func_def_check.py
@@ -1,7 +1,7 @@
 from textwrap import dedent
 
-from verger.ast2dict import ast2dict_from_file, ast2dict_from_str, copy_dictionary_exclude_keys
 from verger import checker
+from verger.ast2dict import ast2dict_from_file, ast2dict_from_str, copy_dictionary_exclude_keys
 from verger.function_def import FunctionDefChecker
 
 
@@ -63,9 +63,53 @@ def test_checker_miss():
     )
     simple_code2 = dedent(
         """
-    def add_one2(a, b):
+    def add_one(a):
         return a+1
     """
     )
     report = list(checker.function_signatures(ast2dict_from_str(simple_code), ast2dict_from_str(simple_code2)))
-    assert len(report) == 2
+    assert len(report) == 1
+    assert report[0][1] == "FunctionDef add_one arguments differ from reference module"
+
+
+def test_checker_new_function():
+    simple_code = dedent(
+        """
+    def add_one(a, b):
+        return a+1
+
+    def nothing_to_see_here():
+        ...
+    """
+    )
+    simple_code2 = dedent(
+        """
+    def add_one(a, b):
+        return a+1
+    """
+    )
+    report = list(checker.function_signatures(ast2dict_from_str(simple_code), ast2dict_from_str(simple_code2)))
+    assert len(report) == 1
+    assert report[0][1] == "FunctionDef nothing_to_see_here does not exist in reference module"
+
+
+def test_checker_removed_function():
+    simple_code = dedent(
+        """
+    def add_one(a, b):
+        return a+1
+
+    """
+    )
+    simple_code2 = dedent(
+        """
+    def i_got_removed():
+        ...
+    
+    def add_one(a, b):
+        return a+1
+    """
+    )
+    report = list(checker.function_signatures(ast2dict_from_str(simple_code), ast2dict_from_str(simple_code2)))
+    assert len(report) == 1
+    assert report[0][1] == "FunctionDef i_got_removed does not exist in current module"
